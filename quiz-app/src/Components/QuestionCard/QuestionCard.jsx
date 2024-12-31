d57715880ef6d40d46a8d2f259fbd98e3d8bae0f
Nour123amer <nouramer295@gmail.com>
2024-12-31 18:07:32 +0200

    quiz app

--- quiz-app/src/Components/QuestionCard/QuestionCard.jsx
+++ quiz-app/src/Components/QuestionCard/QuestionCard.jsx
@@ -1,26 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
-export default function QuestionCard({questionInfo ,goToNextPage ,goToPreviousPage, index}) {
+export default function QuestionCard({questionInfo ,goToNextPage ,goToPreviousPage, index ,handleScoreUpdate}) {
     const navigate = useNavigate();
     const [score ,setScore] = useState(0);
     const [selectedOption ,setSelectedOption] = useState('');
     const {question , options ,answer ,id} = questionInfo;
-    
+
 
     function calcScore(){
-        if (selectedOption === answer){
-            setScore(score+1);
-        }else{
-            setScore(score);
-        }
+        // if (selectedOption === answer){
+        //     setScore(score+1);
+        // }else{
+        //     setScore(score);
+        // }
+
+        const isCorrect = selectedOption === answer ? 1 :0;
+        handleScoreUpdate(isCorrect ,index);
+        goToNextPage();
+
     }
 
-   
+
+
+
+
   return (
     <>
-    <div className='border border-white h-full p-4 rounded-md'>
-        <h2>{question}</h2>
+    <div className='  p-4 rounded-md'>
+        <h2 className='mb-4'>{question}</h2>
         <div>
             {options.map((option,index)=>{
             return   <div key={index}>
@@ -36,9 +44,9 @@ export default function QuestionCard({questionInfo ,goToNextPage ,goToPreviousPa
             })}
         </div>
         {/* <p>{answer}</p> */}
-        <button onClick={calcScore} className='bg-white text-[#601A40] px-2 py-1 rounded-md font-semibold mt-4'>Submit Answer</button>
+        <button   disabled={!selectedOption} onClick={calcScore} className='bg-white text-[#601A40] px-2 py-1 rounded-md font-semibold mt-4'>Submit Answer</button>
         
-        <p className='border-2 border-white text-white px-2 py-1 rounded-md mt-4 text-center my-4'>{`Score: ${score}`}</p>
+        {/* <p className='border-2 border-white text-white px-2 py-1 rounded-md mt-4 text-center my-4'>{`Score: ${score}`}</p> */}
 
       
         <div className='flex justify-between px-6 my-6 '>
@@ -49,9 +57,8 @@ export default function QuestionCard({questionInfo ,goToNextPage ,goToPreviousPa
           <i className="fa-solid fa-chevron-right"></i>
         </button>  
         </div> 
+ 
 
-     
-        {/* <button onClick={navigate('/totalscore')} className='bg-white text-[#601A40] px-2 py-1 rounded-md font-semibold'>NEXT</button> */}
     </div>
     </>
   )
--- quiz-app/src/Pages/Question/Question.jsx
+++ quiz-app/src/Pages/Question/Question.jsx
@@ -1,72 +1,104 @@
 import React, { useEffect, useState } from 'react'
 import QuestionCard from '../../Components/QuestionCard/QuestionCard';
+import { array } from 'yup';
 
-    let questions =[
-        {
-          'id':1,  
-          "question": "What is the capital of France?",
-          "options": ["Berlin", "Madrid", "Paris", "Rome"],
-          "answer": "Paris"
-        },
-        {
-            'id':2,  
-          "question": "Which is the largest planet in our solar system?",
-          "options": ["Earth", "Jupiter", "Mars", "Saturn"],
-          "answer": "Jupiter"
-        },
-        {
-            'id':3,  
-            "question": "Which language does brrowser understand?",
-            "options": ["CSS", "Javascript", "HTML", "CPP"],
-            "answer": "Javascript"
-          },
-           {
-            'id':4,  
-            "question": "Which language is used for styling?",
-         "options": ["CSS", "Javascript", "HTML", "CPP"],
-            "answer": "CSS"
-          }, 
-          {
-            'id':5,  
-            "question": "Which language is used for structure?",
-            "options": ["CSS", "Javascript", "HTML", "CPP"],
-               "answer": "HTML"
-          },
-      ]
+let questions = [
+  {
+    'id': 1,
+    "question": "What is the capital of France?",
+    "options": ["Berlin", "Madrid", "Paris", "Rome"],
+    "answer": "Paris"
+  },
+  {
+    'id': 2,
+    "question": "Which is the largest planet in our solar system?",
+    "options": ["Earth", "Jupiter", "Mars", "Saturn"],
+    "answer": "Jupiter"
+  },
+  {
+    'id': 3,
+    "question": "Which language does brrowser understand?",
+    "options": ["CSS", "Javascript", "HTML", "CPP"],
+    "answer": "Javascript"
+  },
+  {
+    'id': 4,
+    "question": "Which language is used for styling?",
+    "options": ["CSS", "Javascript", "HTML", "CPP"],
+    "answer": "CSS"
+  },
+  {
+    'id': 5,
+    "question": "Which language is used for structure?",
+    "options": ["CSS", "Javascript", "HTML", "CPP"],
+    "answer": "HTML"
+  },
+]
 
 export default function Question() {
-    
-    //   const [question ,setQuestion] = useState(null);
-   
-   const [currentPage ,setCurrentPage] = useState(0);
 
-      function goToNextPage(){
-        setCurrentPage(currentPage =>currentPage+1);
-        console.log('next');
-        
-      }
+  //   const [question ,setQuestion] = useState(null);
 
-      function goToPreviousPage(){
-        setCurrentPage(currentPage=>currentPage-1);
-        console.log('prev');
-        
-      }
+  const [scores, setScores] = useState(new Array(questions.length).fill(0));
+  const [currentPage, setCurrentPage] = useState(0);
 
-      
-      return (
+  function goToNextPage() {
+    if (currentPage < questions.length - 1) {
+      setCurrentPage(currentPage => currentPage + 1);
+      console.log('next');
+    }
+  }
+
+  function goToPreviousPage() {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage => currentPage - 1);
+      console.log('prev');
+    }
+
+  }
+
+  function handleScoreUpdate(score, index) {
+    setScores((prevscores) => {
+      const updatedScore = [...prevscores];
+      updatedScore[index] = score;
+      console.log('scores...');
+
+      return updatedScore
+    });
+  }
+
+  const calculatedTotalScore = () => scores.reduce((total, score) => total + score ,0)
+
+  console.log(calculatedTotalScore());
+
+  // useEffect(() => {
+  //   handleScoreUpdate()
+  // }, []
+  // )
+
+  return (
     <>
-    <div className='border text-white bg-[#601A40] border-gray-500 container md:w-1/3 lg:w-1/4 m-auto my-16 flex px-4 flex-col h-[550px]  py-8 rounded-md'>
-    {/* {question? (question.map((q)=>{
+    
+      <div className=' text-white bg-[#601A40] p-2  container md:w-1/3 lg:w-1/4 m-auto my-16 flex  flex-col h-[550px] rounded-md'>
+        {/* {question? (question.map((q)=>{
         return <QuestionCard questionInfo={q} goToNextPage goToPreviousPage id key={q.id} />
     })):(<h2>Loading ... </h2>)} */}
-    
-    {questions? (
-    <QuestionCard questionInfo={questions[currentPage ]}
-      index={currentPage} key={currentPage} goToNextPage={goToNextPage}
-      goToPreviousPage={goToPreviousPage}  />
+<div className='border-2 border-white h-full rounded-md p-2'>
+        {questions ? (
+          <QuestionCard questionInfo={questions[currentPage]}
+            index={currentPage} key={currentPage}
+             goToNextPage={goToNextPage}
+            handleScoreUpdate={handleScoreUpdate}
+            goToPreviousPage={goToPreviousPage}
+             scores={scores} setScores={setScores}
+            calculatedTotalScore={calculatedTotalScore} />
 
-):(<h2>Loading ... </h2>)}
-    </div>
+        ) : (<h2>Loading ... </h2>)}
+  
+        <p className='border-2 border-white text-white w-3/4 mx-auto px-2 py-1 rounded-md mt-4 text-center my-4'>{calculatedTotalScore()} / {questions.length}</p>
+         </div>
+      </div>
+     
 
     </>
   )
