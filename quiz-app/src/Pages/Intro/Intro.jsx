ffe13b86ef0ca89140249c205ca63399df8294d8
Nour123amer <nouramer295@gmail.com>
2025-01-04 20:00:16 +0200

    Quiz App

--- quiz-app/src/Pages/Intro/Intro.jsx
+++ quiz-app/src/Pages/Intro/Intro.jsx
@@ -8,12 +8,12 @@ export default function Intro() {
 
     return (
         <>
-            <div className='text-[#601A40] border-2 border-gray-400 bg-[#601A40] image flex flex-col h-[550px] md:w-1/3 lg:w-1/4 m-auto my-16 justify-center items-center rounded-md'>
+            <div className='text-[#0492C2]  bg-[#0492C2] image flex flex-col h-[550px] md:w-1/3 lg:w-1/4 m-auto my-16 justify-center items-center rounded-md'>
                 <h2 className='mb-8 text-3xl font-bold'>QUIZ</h2>
                 <p className='font-semibold'>Loading your experience.Please wait ...</p>
 
 
-                <button onClick={()=>{ navigate('/about')}} className=' mx-3 flex gap-4 items-center bg-white text-[#601A40] px-2 py-1 rounded-md font-bold ms-auto mt-16'>
+                <button onClick={()=>{ navigate('/about')}} className=' mx-3 flex gap-4 items-center bg-white text-[#0492C2] px-2 py-1 rounded-md font-bold ms-auto mt-16'>
                     <p>Next</p>
                     <i className="fa-solid fa-arrow-right"></i>
                 </button>
